449762f5afc1b2a3ef6db2f232143e28f24a6a9a
Jay-DVT <diegovt.arbt@gmail.com>
2022-10-11 09:52:00 -0600

    login creation

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
 
+#app config
 app = Flask(__name__)
+
+#session config
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
-    return '<h1> Hello World </h1>'
+    if not session.get("name"):
+        return redirect("/login")
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session["name"] = request.form.get("name")
+        return redirect ("/")
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session["name"] = None
+    return redirect('/')
