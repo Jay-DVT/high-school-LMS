fb2e18c4edb9cf1e4e947549bfa119edd2c9ef2b
Jay-DVT <diegovt.arbt@gmail.com>
2022-10-18 10:17:46 -0600

    Login and Register page creation
    Database connection pending, will be done by tomorrow
        Requires correct variable designation

--- app.py
+++ app.py
@@ -1,27 +1,102 @@
-from flask import Flask, session, render_template, redirect, request, url_for
+from flask import Flask, redirect, url_for, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from os import path
-from flask_login import LoginManager
-from flask_session import Session
+# from os import path
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, SubmitField
+# from wtforms.validators import InputRequired, Length, ValidationError
+from sqlalchemy.sql import func
+from werkzeug.security import generate_password_hash, check_password_hash
 
-#app config
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 app = Flask(__name__)
-#session config
-# Secret key with imported value
-app.config["SECRET_KEY"] = 'secret'
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
+#* login start
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+#* Database Models
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    idValue = db.Column(db.String(150), unique=True)
+    firstName = db.Column(db.String(150))
+    lastName = db.Column(db.String(150))
+    password = db.Column(db.String(150))
+    notes = db.relationship('Note')
+
+#*Routes
 @app.route("/")
 def index():
     #if there is no registered login session, redirect to login page
-    if not session.get("name"):
-        return redirect("/login")
-    return render_template("index.html")
+    return redirect("/login")
 
-from auth import auth
-app.register_blueprint(auth, url_prefix="/")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        idValue = request.form.get('idValue')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(idValue=idValue).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('idValue does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        idValue = request.form.get('idValue')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(idValue=idValue).first()
+        if user:
+            flash('idValue already exists.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(idValue=idValue, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("signup.html", user=current_user)
 
 if __name__ == '__main__':
     #debug mode, turn off in deployment
