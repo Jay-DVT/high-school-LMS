ed13d01d43bf79a475510d1730610005cf8b2757
Jay-DVT <diegovt.arbt@gmail.com>
2022-10-17 21:59:37 -0600

    create initial auth pages

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, session, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 from flask_session import Session
 
 #app config
 app = Flask(__name__)
-
 #session config
+# Secret key with imported value
+app.config["SECRET_KEY"] = 'secret'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 @app.route("/")
 def index():
+    #if there is no registered login session, redirect to login page
     if not session.get("name"):
         return redirect("/login")
     return render_template("index.html")
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        session["name"] = request.form.get("name")
-        return redirect ("/")
-    return render_template('login.html')
+from auth import auth
+app.register_blueprint(auth, url_prefix="/")
 
-@app.route('/logout')
-def logout():
-    session["name"] = None
-    return redirect('/')
+if __name__ == '__main__':
+    #debug mode, turn off in deployment
+    app.run(debug=True)
